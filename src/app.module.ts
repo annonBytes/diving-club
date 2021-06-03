6c54cac1372eb7ab6bf50e149562a635969a485a
Ockiya Daniel <beinmonyudaniel@gmail.com>
2021-06-03 23:14:47 +0200

    Dockerfile added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {GraphQLModule} from "@nestjs/graphql";
+
 
 @Module({
-  imports: [
-      GraphQLModule.forRoot({}),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
